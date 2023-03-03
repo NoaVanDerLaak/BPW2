14de0de711b7adc6df2861b8f81e915ba4cb463d
Noa <18129@ma-web.nl>
2023-03-03 19:42:45 +0100

    Portal finetune & post process setup

--- Assets/Scripts/PortalTextureSetup.cs
+++ Assets/Scripts/PortalTextureSetup.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class PortalTextureSetup : MonoBehaviour
 {
+    public Camera camera1;
     public Camera camera2;
 
+    public Material camera1MAT;
     public Material camera2MAT;
 
     void Start()
     {
+        if (camera1.targetTexture != null)
+        {
+            camera1.targetTexture.Release();
+        }    
+        camera1.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        camera1MAT.mainTexture = camera1.targetTexture;
+        
         if (camera2.targetTexture != null)
         {
             camera2.targetTexture.Release();
